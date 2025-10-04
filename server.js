9bfb4e86d39b5710766792383ec4564ffa700640
Hass <hasseneafif@gmail.com>
2025-10-04 14:56:22 +0200

    Simple cors

--- server.js
+++ server.js
@@ -13,17 +13,7 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 // Middleware
-const allowedOrigins = ['https://ailex.pro', 'https://www.ailex.pro'];
-app.use(cors({
-  origin: function (origin, callback) {
-    if (!origin || allowedOrigins.includes(origin)) {
-      callback(null, true);
-    } else {
-      callback(new Error('Not allowed by CORS'));
-    }
-  },
-  credentials: true
-}));
+app.use(cors());
 app.use(express.json({ limit: '10mb' }));
 app.use(express.urlencoded({ extended: true, limit: '10mb' }));
 
